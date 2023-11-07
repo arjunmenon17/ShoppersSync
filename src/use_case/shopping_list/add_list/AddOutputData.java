d9e813d2d2ef2496d4e18352c0936273b1e10dd0
Rohan Regi <rohanregi@rohans-air-2.lan>
2023-11-06 23:21:28 -0500

    Implemented AddOutputDataBoundary

--- src/use_case/shopping_list/add_list/AddOutputData.java
+++ src/use_case/shopping_list/add_list/AddOutputData.java
@@ -1,4 +1,5 @@
 package use_case.shopping_list.add_list;
 
 public class AddOutputData {
+    // No output data for now
 }
--- src/use_case/shopping_list/add_list/AddOutputDataBoundary.java
+++ src/use_case/shopping_list/add_list/AddOutputDataBoundary.java
@@ -1,4 +1,5 @@
 package use_case.shopping_list.add_list;
 
 public interface AddOutputDataBoundary {
+    void presentResult(AddOutputData outputData);
 }
