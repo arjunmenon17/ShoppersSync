f74c76199b8911a13b5bc440e5bf2049ebecf16c
Rohan Regi <rohanregi@rohans-air-2.lan>
2023-11-06 23:19:51 -0500

    Implemented AddInteractor

--- src/use_case/shopping_list/add_list/AddInteractor.java
+++ src/use_case/shopping_list/add_list/AddInteractor.java
@@ -1,4 +1,13 @@
 package use_case.shopping_list.add_list;
 
-public class AddInteractor {
+public class AddInteractor implements AddInputBoundary{
+    private final AddDataAccessInterface dataAccess;
+
+    public AddInteractor(AddDataAccessInterface dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+    @Override
+    public void addProductToShoppingList(AddInputData input) {
+        dataAccess.addProductToShoppingList(input.getProduct());
+    }
 }
