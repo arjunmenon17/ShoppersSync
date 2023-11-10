25cf44b361688a1546e5eadb3aa65b8ce4d46698
Rohan Regi <rohanjregi@gmail.com>
2023-11-10 16:08:21 -0500

    Cleared ShoppingListView.java

---
+++ src/view/ShoppingListView.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingListView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    public ShoppingListView() {
+
+    }
+
+    /**
+     * React to a button click that results in evt.
+     */
+    public void actionPerformed(ActionEvent evt) {
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+    }
+}
