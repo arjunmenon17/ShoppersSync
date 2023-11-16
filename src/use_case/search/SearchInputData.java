c5d68816133cb42fe152b674a37cf149e3a7e9eb
minhasir <irven.minhas@mail.utoronto.ca>
2023-11-16 18:16:37 -0500

    Search Controller changes

--- src/use_case/search/SearchInputData.java
+++ src/use_case/search/SearchInputData.java
@@ -2,4 +2,13 @@ package use_case.search;
 
 public class SearchInputData {
 
+    final private String id;
+
+    public SearchInputData(String id) {
+
+        this.id = id;
+
+    }
+
+    String getId() {return id;}
 }
