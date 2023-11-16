6b2bd67a87560d8e4ac9d0608bb39ae25bf8f7f9
minhasir <irven.minhas@mail.utoronto.ca>
2023-11-16 17:33:01 -0500

    SearchView added

--- src/view/Basepage.java
+++ src/view/Basepage.java
@@ -3,15 +3,25 @@ package view;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class Basepage {
-    private JPanel mainPanel;
+public class Basepage extends JFrame {
+    public JPanel panelMain;
     private JTextField searchInputField;
     private JButton searchButton;
     private JButton addButton;
+    private JPanel productPanel;
+    private JButton searchClearTextButton;
 
 
     public Basepage() {
+
+        setContentPane(panelMain);
+        setTitle("ShopperSYNC");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1024, 600 );
+        setVisible(true);
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -21,6 +31,19 @@ public class Basepage {
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        searchInputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+            }
+        });
+        searchClearTextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                searchInputField.setText("");
 
             }
         });
