101653dc7a2856f07da49c1629f7d4367689e59f
minhasir <irven.minhas@mail.utoronto.ca>
2023-11-16 17:33:01 -0500

    SearchView added

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,8 @@
 package app;
 
+import view.Basepage;
+
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,25 +13,35 @@ import java.net.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String urlString = "https://api.bluecartapi.com/request?api_key=17E691EE2B274440B026E21B2DE8CE76&type=product&item_id=782866746";
-        URL fitbit  = new URL(urlString);
-        URLConnection conn = fitbit.openConnection();
-        InputStream stuff = conn.getInputStream();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(stuff));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
+        // The main application window
+        JFrame application = new Basepage();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
 
-        System.out.println(response.toString());
+    }
+
+
+    public static void temp(String[]args) throws IOException {
+            String urlString = "https://api.bluecartapi.com/request?api_key=17E691EE2B274440B026E21B2DE8CE76&type=product&item_id=782866746";
+            URL fitbit  = new URL(urlString);
+            URLConnection conn = fitbit.openConnection();
+            InputStream stuff = conn.getInputStream();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(stuff));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response.toString());
 
             // print result
             System.out.println(response.toString());
 
 
+        }
+
     }
-}
