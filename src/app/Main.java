85cbe8bd39422d9c3559784928fe5e423426ebbd
minhasir <irven.minhas@mail.utoronto.ca>
2023-11-16 17:42:27 -0500

    SearchView added

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
-import view.Basepage;
+import view.SearchView;
+import view.SearchView;
 
 import javax.swing.*;
 import java.io.BufferedReader;
@@ -15,7 +16,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         // The main application window
-        JFrame application = new Basepage();
+        JFrame application = new SearchView();
 
 
     }
