a41d7c82198c407fb480de344ef86f88324cc1ea
minhasir <irven.minhas@mail.utoronto.ca>
2023-11-16 22:24:23 -0500

    image added

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,9 @@
 package app;
 
+import interface_adapter.Search.SearchViewModel;
+import interface_adapter.shopping_list.ShoppingListViewModel;
 import view.SearchView;
+import view.ShoppingListView;
 
 import javax.swing.*;
 
@@ -8,10 +11,9 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        // The main application window
-        JFrame application = new SearchView();
-
-
+        ShoppingListViewModel shoppingListViewModel = new ShoppingListViewModel();
+        new ShoppingListView(shoppingListViewModel);
+        SearchViewModel searchViewModel = new SearchViewModel();
+        new SearchView(searchViewModel);
     }
 }
--- src/view/SearchView.java
+++ src/view/SearchView.java
@@ -1,6 +1,7 @@
 package view;
 
 import interface_adapter.Search.SearchController;
+import interface_adapter.Search.SearchViewModel;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -14,39 +15,45 @@ public class SearchView extends JFrame {
     private JPanel productPanel;
     private JButton searchClearTextButton;
 
+    private SearchViewModel viewModel;
+
     private final SearchController searchController;
 
-
-    public SearchView(SearchController searchController) {
+    public SearchView(SearchViewModel viewModel, SearchController searchController) {
+        this.viewModel = viewModel;
         this.searchController = searchController;
-
 
         setContentPane(panelMain);
         setTitle("ShopperSYNC");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1024, 600 );
+        setSize(1024, 600);
         setVisible(true);
+
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //viewModel.setSearchInput(searchInputField.getText());
+                //viewModel.firePropertyChanged();
                 if (e.getSource().equals(searchButton)) {
 
                     searchController.execute(searchInputField.getText());
-
                 }
+
+
             }
         });
+
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                viewModel.firePropertyChanged();
             }
         });
+
         searchClearTextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 searchInputField.setText("");
-
             }
         });
     }
