11fff7f18af9032f80c2f4c9f9fd0b272bf2cf03
Arjun Menon <arjun.mnn17@gmail.com>
2023-12-01 23:27:36 -0500

    Completed untill required usage of CalcScoreDataAccessInterface in the execute function()

--- src/use_case/search/calc_score/CalcScoreInteractor.java
+++ src/use_case/search/calc_score/CalcScoreInteractor.java
@@ -1,4 +1,16 @@
 package use_case.search.calc_score;
 
-public class CalcScoreInteractor {
+public class CalcScoreInteractor implements CalcScoreInputBoundary{
+    final CalcScoreOutputBoundary calcScoreOutputBoundary;
+    final CalcScoreDataAccessInterface calcScoreDataAccessInterface;
+
+    public CalcScoreInteractor (CalcScoreOutputBoundary calcScoreOutputBoundary, CalcScoreDataAccessInterface calcScoreDataAccessInterface) {
+        this.calcScoreOutputBoundary = calcScoreOutputBoundary;
+        this.calcScoreDataAccessInterface = calcScoreDataAccessInterface;
+    }
+    @Override
+    public void execute(CalcScoreInputData calcScoreInputData) {
+        String company = calcScoreInputData.getCompany();
+
+    }
 }
