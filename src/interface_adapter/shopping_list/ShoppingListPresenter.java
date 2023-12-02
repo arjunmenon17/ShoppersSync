db663d042213614187fa93339e0351ec2ee2e811
Rohan Regi <rohanjregi@gmail.com>
2023-12-02 12:01:56 -0500

    Implemented ShoppingListPresenter and ShoppingListState

--- src/interface_adapter/shopping_list/ShoppingListPresenter.java
+++ src/interface_adapter/shopping_list/ShoppingListPresenter.java
@@ -1,4 +1,28 @@
 package interface_adapter.shopping_list;
 
-public class ShoppingListPresenter {
+import entity.Product;
+import use_case.shopping_list.add.AddOutputBoundary;
+
+public class ShoppingListPresenter implements AddOutputBoundary {
+    private final ShoppingListViewModel shoppingListViewModel;
+
+    public ShoppingListPresenter(ShoppingListViewModel shoppingListViewModel) {
+        this.shoppingListViewModel = shoppingListViewModel;
+    }
+
+
+    public void prepareSuccessView(Product newProduct) {
+        ShoppingListState newState = shoppingListViewModel.getState();
+        newState.addProduct(newProduct);
+        shoppingListViewModel.setState(newState);
+        shoppingListViewModel.firePropertyChanged();
+    }
+
+
+    public void prepareFailView(String error) {
+        ShoppingListState newState = new ShoppingListState();
+        newState.setErrorMessage(error);
+        shoppingListViewModel.setState(newState);
+        shoppingListViewModel.firePropertyChanged();
+    }
 }
--- src/interface_adapter/shopping_list/ShoppingListState.java
+++ src/interface_adapter/shopping_list/ShoppingListState.java
@@ -1,3 +1,32 @@
 package interface_adapter.shopping_list;
+
+import entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingListState {
+    private List<Product> productList;
+    private String errorMessage;
+
+    public ShoppingListState() {
+        this.productList = new ArrayList<>();
+        this.errorMessage = null;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
