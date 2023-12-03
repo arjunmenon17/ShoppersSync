1058c714570801b805afebcf5f4d9ddbd198fb50
Arjun Menon <arjun.mnn17@gmail.com>
2023-12-03 04:14:05 -0500

    worked on checkoutController class and encountered some implementation issues

--- src/interface_adapter/shopping_list/ShoppingListController.java
+++ src/interface_adapter/shopping_list/ShoppingListController.java
@@ -3,6 +3,9 @@ package interface_adapter.shopping_list;
 import entity.Product;
 import use_case.shopping_list.add.AddInputData;
 
+import java.util.List;
+
 public interface ShoppingListController {
     public void execute(Product product); // For add and remove
+    public void execute_check_out(List<Product> products);
 }
