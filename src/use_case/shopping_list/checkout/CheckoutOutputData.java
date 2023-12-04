8408d858aad5e71cdc55c706dbf7e9e122658e03
Arjun Menon <arjun.mnn17@gmail.com>
2023-12-03 23:34:05 -0500

    finished CheckoutOutputData

--- src/use_case/shopping_list/checkout/CheckoutOutputData.java
+++ src/use_case/shopping_list/checkout/CheckoutOutputData.java
@@ -4,10 +4,14 @@ import entity.Product;
 
 public class CheckoutOutputData {
     private final float total_score;
-    private boolean useCaseFailed;
+    private final boolean useCaseFailed;
 
     public CheckoutOutputData(float total_score, boolean useCaseFailed) {
         this.total_score = total_score;
         this.useCaseFailed = useCaseFailed;
+    }
+
+    public float getTotal_score() {
+        return total_score;
     }
 }
